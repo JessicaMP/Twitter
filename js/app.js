9f87480f82e48e7ac50edf9511c4caed1fddf777
JessicaMP <jessica95.mp@gmail.com>
2017-11-18 15:37:16 -0500

    Versión 0.0.4

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var contador = 140;
 var button = document.getElementById('button');
 var counter = document.getElementsByClassName('counter')[0];
 var container = document.getElementById('container');
+var boxRight = document.getElementsByClassName('box-right')[0];
 //Agregando evento click al boton para q te retorne el contenido ingresado
 button.addEventListener('click', function(event) {
     var firstBox = document.createElement('div');
@@ -16,30 +17,33 @@ if (text.value == false) {
 }else{
   firstBox.appendChild(content);
   container.appendChild(firstBox);
+
     text.value = '';
   }
 });
 
-//button.removeEventListener('click', function(event));
-
 //Cambio de color del boton al ingresar texto
 text.addEventListener('keyup', function() {
   var contenido = text.value;
-  if (contenido == '' || contenido === ' ') {
-    button.classList.remove('buttonColor');
-
+  var length = text.value.lenght;
+  if (contenido == '') {
+    button.className = 'Original';
   }else {
-    button.classList.add('buttonColor');
+    button.className = 'buttonColor';
+    text.style.height = '80px';
+    text.style.height = text.scrollHeight + 'px';
+    boxRight.style.height = '200px';
+    boxRight.style.height = boxRight.scrollHeight + 'px';
   }
   //Agregando el contador
   var letters = text.value.length;
   var restaContador = 140 - letters;
   counter.textContent = restaContador;
-  //Agregando colores a los caracteres
-}) ;
+});
+
+//Agregando colores a los caracteres
 text.addEventListener('keyup', function() {
   var letters = text.value.length;
-  var negative = -1;
   if (letters >= 0 && letters <= 120) {
   counter.className = 'original';
 } else if(letters = 122 && letters <= 130){
@@ -48,3 +52,13 @@ text.addEventListener('keyup', function() {
   counter.className = 'red';
 }
 }) ;
+
+//Desabilitar el boton cuando pasa de los 140 caracteres
+text.addEventListener('keyup', function() {
+  var letters = text.value.length;
+  if (letters > 140) {
+    button.disabled = true;
+  }else {
+    button.disabled = false;
+  }
+});
